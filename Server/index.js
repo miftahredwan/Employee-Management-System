76974ee2f8f6bcaacb6d73b7111cc1b81b904dd2
mihastech <mihastechdeveloper@gmail.com>
2024-05-25 16:48:40 +0300

    "Updated CORS configuration in Server/index.js and removed Vercel configuration file"

--- Server/index.js
+++ Server/index.js
@@ -6,16 +6,16 @@ import Jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser"
 
 const app = express()
-// app.use(cors({
-//     origin:["http://localhost:5173"],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true
-// }))
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin:["http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-}));
+}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }));
 
 app.use(express.json())
 app.use(cookieParser())
