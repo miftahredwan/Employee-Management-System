92c4ed064a1fd798227fae71371eadd46178dcd9
mihastech <mihastechdeveloper@gmail.com>
2024-09-07 21:13:00 +0300

    responsive

--- EmployeeMs/src/Components/Start.jsx
+++ EmployeeMs/src/Components/Start.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axiosBase from "../axios.config";
-
+import './styles.css';
 
 const Start = () => {
     const navigate = useNavigate()
@@ -23,13 +23,13 @@ const Start = () => {
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
-      <div className="p-3 rounded w-25 border loginForm">
+      <div className="p-3 rounded w-25 border loginForm w-100" style={{ maxWidth: '400px' }}>
         <h2 className="text-center">Login As</h2>
         <div className="d-flex justify-content-between mt-5 mb-2">
-          <button type="button" className="btn btn-primary" onClick={() => {navigate('/employee_login')}}>
+          <button type="button" className="btn btn-primary me-2" onClick={() => {navigate('/employee_login')}}>
             Employee
           </button>
-          <button type="button" className="btn btn-success" onClick={() => {navigate('/adminlogin')}}>
+          <button type="button" className="btn btn-success ms-2" onClick={() => {navigate('/adminlogin')}}>
             Admin
           </button>
         </div>
