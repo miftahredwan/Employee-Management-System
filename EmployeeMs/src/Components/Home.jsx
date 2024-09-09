86c9351ba71da12b2ffccbb51a22b19fb7b65f38
mihastech <mihastechdeveloper@gmail.com>
2024-09-09 12:05:08 +0300

    commit 10

--- EmployeeMs/src/Components/Home.jsx
+++ EmployeeMs/src/Components/Home.jsx
@@ -66,31 +66,31 @@ const Home = () => {
     <div>
   <div className='p-10 d-flex flex-wrap justify-content-around mt-3 w-100 w-lg-25'>
     <div className='px-3 pt-2 pb-3 border shadow-sm w-100 w-md-50 w-lg-15 mb-3'>
-      <div className='text-start pb-1'>
+      <div className='text-center pb-1'>
         <h4>Admin</h4>
       </div>
       <hr />
-      <div className='d-flex gap-5'>
+      <div className='d-flex gap-5 justify-content-center'>
         <h5>Total:</h5>
         <h5>{adminTotal}</h5>
       </div>
     </div>
-    <div className='px-3 pt-2 pb-3 border shadow-sm w-100 w-md-50 w-lg-15 mb-3'>
-      <div className='text-start pb-1'>
+    <div className='px-+10 pt-2 pb-3 border shadow-sm w-100 w-md-50 w-lg-15 mb-3'>
+      <div className='text-center pb-1'>
         <h4>Employee</h4>
       </div>
       <hr />
-      <div className='d-flex gap-5'>
+      <div className='d-flex gap-5 justify-content-center'>
         <h5>Total:</h5>
         <h5>{employeeTotal}</h5>
       </div>
     </div>
     <div className='px-3 pt-2 pb-3 border shadow-sm w-100 w-md-50 w-lg-15 mb-3'>
-      <div className='text-start pb-1'>
+      <div className='text-center pb-1'>
         <h4>Salary</h4>
       </div>
       <hr />
-      <div className='d-flex gap-5 '>
+      <div className='d-flex gap-5 justify-content-center'>
         <h5>Total:</h5>
         <h5>{salaryTotal ? `${salaryTotal} ETB` : '0 ETB'}</h5>
       </div>
