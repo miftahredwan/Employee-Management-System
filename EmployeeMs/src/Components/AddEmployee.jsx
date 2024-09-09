344bcfa994f95ddd3fcad9c788a38f66f18cf1e4
mihastech <mihastechdeveloper@gmail.com>
2024-09-09 14:44:44 +0300

    commit 10

--- EmployeeMs/src/Components/AddEmployee.jsx
+++ EmployeeMs/src/Components/AddEmployee.jsx
@@ -245,134 +245,234 @@ const AddEmployee = () => {
   };
 
   return (
+    // <div className="container mt-3">
+    //   <div className="p-3 d-flex justify-content-center">
+    //     <div className="p-3 rounded border w-100 w-md-75 w-lg-25">
+    //       <h3 className="text-center">Add Employee</h3>
+    //       {loading ? (
+    //         <div className="text-center">
+    //           <ClipLoader color="#123abc" loading={loading} size={50} />
+    //         </div>
+    //       ) : (
+    //         <form className="row g-3" onSubmit={handleSubmit}>
+    //           <div className="col-12">
+    //             <label htmlFor="inputName" className="form-label">
+    //               Name
+    //             </label>
+    //             <input
+    //               type="text"
+    //               className="form-control rounded-0"
+    //               id="inputName"
+    //               placeholder="Enter Name"
+    //               onChange={(e) =>
+    //                 setEmployee({ ...employee, name: e.target.value })
+    //               }
+    //             />
+    //           </div>
+    //           <div className="col-12">
+    //             <label htmlFor="inputEmail4" className="form-label">
+    //               Email
+    //             </label>
+    //             <input
+    //               type="email"
+    //               className="form-control rounded-0"
+    //               id="inputEmail4"
+    //               placeholder="Enter Email"
+    //               autoComplete="off"
+    //               onChange={(e) =>
+    //                 setEmployee({ ...employee, email: e.target.value })
+    //               }
+    //             />
+    //           </div>
+    //           <div className="col-12">
+    //             <label htmlFor="inputPassword4" className="form-label">
+    //               Password
+    //             </label>
+    //             <input
+    //               type="password"
+    //               className="form-control rounded-0"
+    //               id="inputPassword4"
+    //               placeholder="Enter Password"
+    //               onChange={(e) =>
+    //                 setEmployee({ ...employee, password: e.target.value })
+    //               }
+    //             />
+    //           </div>
+    //           <div className="col-12">
+    //             <label htmlFor="inputSalary" className="form-label">
+    //               Salary
+    //             </label>
+    //             <input
+    //               type="text"
+    //               className="form-control rounded-0"
+    //               id="inputSalary"
+    //               placeholder="Enter Salary"
+    //               autoComplete="off"
+    //               onChange={(e) =>
+    //                 setEmployee({ ...employee, salary: e.target.value })
+    //               }
+    //             />
+    //           </div>
+    //           <div className="col-12">
+    //             <label htmlFor="inputAddress" className="form-label">
+    //               Address
+    //             </label>
+    //             <input
+    //               type="text"
+    //               className="form-control rounded-0"
+    //               id="inputAddress"
+    //               placeholder="1234 Main St"
+    //               autoComplete="off"
+    //               onChange={(e) =>
+    //                 setEmployee({ ...employee, address: e.target.value })
+    //               }
+    //             />
+    //           </div>
+    //           <div className="col-12">
+    //             <label htmlFor="category" className="form-label">
+    //               Category
+    //             </label>
+    //             <select
+    //               name="category"
+    //               id="category"
+    //               className="form-select"
+    //               value={employee.category_id}
+    //               onChange={(e) =>
+    //                 setEmployee({ ...employee, category_id: e.target.value })
+    //               }
+    //             >
+    //               <option value="">Select a category</option>
+    //               {category.map((c) => (
+    //                 <option key={c.id} value={c.id}>
+    //                   {c.name}
+    //                 </option>
+    //               ))}
+    //             </select>
+    //           </div>
+    //           <div className="col-12 mb-3">
+    //             <label className="form-label" htmlFor="inputGroupFile01">
+    //               Select Image
+    //             </label>
+    //             <input
+    //               type="file"
+    //               className="form-control rounded-0"
+    //               id="inputGroupFile01"
+    //               name="image"
+    //               onChange={(e) =>
+    //                 setEmployee({ ...employee, image: e.target.files[0] })
+    //               }
+    //             />
+    //           </div>
+    //           <div className="col-12">
+    //             <button type="submit" className="btn btn-primary w-100">
+    //               Add Employee
+    //             </button>
+    //           </div>
+    //         </form>
+    //       )}
+    //     </div>
+    //   </div>
+    // </div>
+
     <div className="container mt-3">
-      <div className="p-3 d-flex justify-content-center">
-        <div className="p-3 rounded border w-100 w-md-75 w-lg-25">
-          <h3 className="text-center">Add Employee</h3>
-          {loading ? (
-            <div className="text-center">
-              <ClipLoader color="#123abc" loading={loading} size={50} />
+  <div className="row justify-content-center">
+    <div className="col-12 col-md-8 col-lg-6">
+      <div className="p-3 rounded border">
+        <h3 className="text-center">Add Employee</h3>
+        {loading ? (
+          <div className="text-center">
+            <ClipLoader color="#123abc" loading={loading} size={50} />
+          </div>
+        ) : (
+          <form className="row g-3" onSubmit={handleSubmit}>
+            <div className="col-12">
+              <label htmlFor="inputName" className="form-label">Name</label>
+              <input
+                type="text"
+                className="form-control rounded-0"
+                id="inputName"
+                placeholder="Enter Name"
+                onChange={(e) => setEmployee({ ...employee, name: e.target.value })}
+              />
             </div>
-          ) : (
-            <form className="row g-3" onSubmit={handleSubmit}>
-              <div className="col-12">
-                <label htmlFor="inputName" className="form-label">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  className="form-control rounded-0"
-                  id="inputName"
-                  placeholder="Enter Name"
-                  onChange={(e) =>
-                    setEmployee({ ...employee, name: e.target.value })
-                  }
-                />
-              </div>
-              <div className="col-12">
-                <label htmlFor="inputEmail4" className="form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  className="form-control rounded-0"
-                  id="inputEmail4"
-                  placeholder="Enter Email"
-                  autoComplete="off"
-                  onChange={(e) =>
-                    setEmployee({ ...employee, email: e.target.value })
-                  }
-                />
-              </div>
-              <div className="col-12">
-                <label htmlFor="inputPassword4" className="form-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  className="form-control rounded-0"
-                  id="inputPassword4"
-                  placeholder="Enter Password"
-                  onChange={(e) =>
-                    setEmployee({ ...employee, password: e.target.value })
-                  }
-                />
-              </div>
-              <div className="col-12">
-                <label htmlFor="inputSalary" className="form-label">
-                  Salary
-                </label>
-                <input
-                  type="text"
-                  className="form-control rounded-0"
-                  id="inputSalary"
-                  placeholder="Enter Salary"
-                  autoComplete="off"
-                  onChange={(e) =>
-                    setEmployee({ ...employee, salary: e.target.value })
-                  }
-                />
-              </div>
-              <div className="col-12">
-                <label htmlFor="inputAddress" className="form-label">
-                  Address
-                </label>
-                <input
-                  type="text"
-                  className="form-control rounded-0"
-                  id="inputAddress"
-                  placeholder="1234 Main St"
-                  autoComplete="off"
-                  onChange={(e) =>
-                    setEmployee({ ...employee, address: e.target.value })
-                  }
-                />
-              </div>
-              <div className="col-12">
-                <label htmlFor="category" className="form-label">
-                  Category
-                </label>
-                <select
-                  name="category"
-                  id="category"
-                  className="form-select"
-                  value={employee.category_id}
-                  onChange={(e) =>
-                    setEmployee({ ...employee, category_id: e.target.value })
-                  }
-                >
-                  <option value="">Select a category</option>
-                  {category.map((c) => (
-                    <option key={c.id} value={c.id}>
-                      {c.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="col-12 mb-3">
-                <label className="form-label" htmlFor="inputGroupFile01">
-                  Select Image
-                </label>
-                <input
-                  type="file"
-                  className="form-control rounded-0"
-                  id="inputGroupFile01"
-                  name="image"
-                  onChange={(e) =>
-                    setEmployee({ ...employee, image: e.target.files[0] })
-                  }
-                />
-              </div>
-              <div className="col-12">
-                <button type="submit" className="btn btn-primary w-100">
-                  Add Employee
-                </button>
-              </div>
-            </form>
-          )}
-        </div>
+            <div className="col-12">
+              <label htmlFor="inputEmail4" className="form-label">Email</label>
+              <input
+                type="email"
+                className="form-control rounded-0"
+                id="inputEmail4"
+                placeholder="Enter Email"
+                autoComplete="off"
+                onChange={(e) => setEmployee({ ...employee, email: e.target.value })}
+              />
+            </div>
+            <div className="col-12">
+              <label htmlFor="inputPassword4" className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control rounded-0"
+                id="inputPassword4"
+                placeholder="Enter Password"
+                onChange={(e) => setEmployee({ ...employee, password: e.target.value })}
+              />
+            </div>
+            <div className="col-12">
+              <label htmlFor="inputSalary" className="form-label">Salary</label>
+              <input
+                type="text"
+                className="form-control rounded-0"
+                id="inputSalary"
+                placeholder="Enter Salary"
+                autoComplete="off"
+                onChange={(e) => setEmployee({ ...employee, salary: e.target.value })}
+              />
+            </div>
+            <div className="col-12">
+              <label htmlFor="inputAddress" className="form-label">Address</label>
+              <input
+                type="text"
+                className="form-control rounded-0"
+                id="inputAddress"
+                placeholder="1234 Main St"
+                autoComplete="off"
+                onChange={(e) => setEmployee({ ...employee, address: e.target.value })}
+              />
+            </div>
+            <div className="col-12">
+              <label htmlFor="category" className="form-label">Category</label>
+              <select
+                name="category"
+                id="category"
+                className="form-select"
+                value={employee.category_id}
+                onChange={(e) => setEmployee({ ...employee, category_id: e.target.value })}
+              >
+                <option value="">Select a category</option>
+                {category.map((c) => (
+                  <option key={c.id} value={c.id}>{c.name}</option>
+                ))}
+              </select>
+            </div>
+            <div className="col-12 mb-3">
+              <label className="form-label" htmlFor="inputGroupFile01">Select Image</label>
+              <input
+                type="file"
+                className="form-control rounded-0"
+                id="inputGroupFile01"
+                name="image"
+                onChange={(e) => setEmployee({ ...employee, image: e.target.files[0] })}
+              />
+            </div>
+            <div className="col-12">
+              <button type="submit" className="btn btn-primary w-100">Add Employee</button>
+            </div>
+          </form>
+        )}
       </div>
     </div>
+  </div>
+</div>
+
   );
 };
 
